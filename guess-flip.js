6254ae6ca726610eaf569ae1d848c1fb51a021ba
uncjackg <jackg@live.unc.edu>
2022-03-10 16:01:43 -0500

    newline?

--- guess-flip.js
+++ guess-flip.js
@@ -5,7 +5,7 @@ const args = minimist(process.argv.slice(1));
 const call = args['call'];
 
 if (!call) {
-  console.error('Error: no input.\nUsage: node guess-flip --call=[heads|tails]\n');
+  console.error('Error: no input.\nUsage: node guess-flip --call=[heads|tails]');
 } else if (call != 'heads' && call != 'tails') {
   console.error('Usage: node guess-flip --call=[heads|tails]');
 } else {
