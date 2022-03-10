e7f0eb53b68793f612e8fcc970192fb890974d67
uncjackg <jackg@live.unc.edu>
2022-03-10 15:44:12 -0500

    fix guess flip error msg

--- guess-flip.js
+++ guess-flip.js
@@ -4,9 +4,10 @@ import minimist from 'minimist';
 const args = minimist(process.argv.slice(1));
 const call = args['call'];
 
-if (call != 'heads' && call != 'tails') {
+if (!call) {
   console.error('Error: no input.\nUsage: node guess-flip --call=[heads|tails]\n');
-  //process.exit(1);
+} else if (call != 'heads' && call != 'tails') {
+  console.error('Usage: node guess-flip --call=[heads|tails]\n');
 } else {
   console.log(flipACoin(call))
 }
