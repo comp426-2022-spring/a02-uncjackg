d229af480dacbc6e5bc2062811a359cfe25ab03c
uncjackg <jackg@live.unc.edu>
2022-04-17 16:26:34 -0400

    put it back

--- guess-flip.js
+++ guess-flip.js
@@ -4,10 +4,10 @@ import minimist from 'minimist';
 const args = minimist(process.argv.slice(1));
 const call = args['call'];
 
-if (!call || (call != 'heads' && call != 'tails')) {
+if (!call) {
   console.error('Error: no input.\nUsage: node guess-flip --call=[heads|tails]');
-// } else if (call != 'heads' && call != 'tails') {
-//   console.error('Usage: node guess-flip --call=[heads|tails]');
+} else if (call != 'heads' && call != 'tails') {
+  console.error('Usage: node guess-flip --call=[heads|tails]');
 } else {
   console.log(flipACoin(call))
 }
