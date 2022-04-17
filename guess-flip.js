81f85b63370f21339d4bb9f0d68163d52851de2c
uncjackg <jackg@live.unc.edu>
2022-04-17 16:21:22 -0400

    test

--- guess-flip.js
+++ guess-flip.js
@@ -6,8 +6,8 @@ const call = args['call'];
 
 if (!call) {
   console.error('Error: no input.\nUsage: node guess-flip --call=[heads|tails]');
-} else if (call != 'heads' && call != 'tails') {
-  console.error('Usage: node guess-flip --call=[heads|tails]');
+// } else if (call != 'heads' && call != 'tails') {
+//   console.error('Usage: node guess-flip --call=[heads|tails]');
 } else {
   console.log(flipACoin(call))
 }
