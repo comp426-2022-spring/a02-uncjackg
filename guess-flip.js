4e21135d8b579b495e6635af2b1e9527fcadd763
uncjackg <jackg@live.unc.edu>
2022-04-23 15:10:35 -0400

    fix error message

--- guess-flip.js
+++ guess-flip.js
@@ -4,10 +4,8 @@ import minimist from 'minimist';
 const args = minimist(process.argv.slice(1));
 const call = args['call'];
 
-if (!call) {
+if (!call || (call != 'heads' && call != 'tails')) {
   console.error('Error: no input.\nUsage: node guess-flip --call=[heads|tails]');
-} else if (call != 'heads' && call != 'tails') {
-  console.error('Usage: node guess-flip --call=[heads|tails]');
 } else {
   console.log(flipACoin(call))
 }
